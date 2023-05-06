7b8ca22f1126af3916fc9958b8d90aa9eef7ae44
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-05-06 12:42:17 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
 <template>
-  <header>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </header>
-
   <RouterView />
 </template>
