1d966d9b239352a4d31e731598d3ca15d9538c53
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-05-06 12:42:33 +0530

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,29 @@
 <template>
+  <header class="shadow py-2 px-4">
+    <h1>Find Class</h1>
+  </header>
   <main>
-    
+    <div class="flex p-4 items-center flex-col justify-between h-full">
+      <AppSearch />
+      <AppDropdown :options="semesters" selectedValue="Select Semester" />
+    </div>
   </main>
 </template>
+
+<script>
+import AppSearch from '@/components/AppSearch.vue'
+import AppDropdown from '../components/AppDropdown.vue'
+import { semesters } from '../data/data'
+export default {
+  name: 'HomeView',
+  components: {
+    AppSearch,
+    AppDropdown
+  },
+  data() {
+    return {
+      semesters
+    }
+  }
+}
+</script>
