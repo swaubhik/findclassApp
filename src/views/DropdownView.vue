65ae53f675a2e5d019189f8af78d196b91e20c69
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-05-08 21:39:02 +0530

    Create DropdownView.vue

---
+++ src/views/DropdownView.vue
@@ -0,0 +1,35 @@
+<template>
+  <multilevel-dropdown :options="faculties" @selected="onOptionSelected" :keep-open="true" />
+</template>
+
+<script>
+import MultilevelDropdown from '../components/MultilevelDropdown.vue'
+import { semesters, faculties } from '../data/data'
+export default {
+  components: {
+    MultilevelDropdown
+  },
+  data() {
+    return {
+      semesters,
+      faculties: faculties.map((faculty) => {
+        return {
+          ...faculty,
+          // pass semester name and id to the faculty.semesters array
+          semesters: semesters.map((semester) => {
+            return {
+              name: semester.name,
+              id: semester.id
+            }
+          })
+        }
+      })
+    }
+  },
+  methods: {
+    onOptionSelected(option) {
+      console.log('Selected option:', option)
+    }
+  }
+}
+</script>
