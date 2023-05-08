041551c800fdbef31077da2ccbce82d3b8b0cc9e
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-05-08 21:39:01 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/RoutineView.vue')
+    },
+    {
+      path: '/teacher/:code/:sem',
+      name: 'teacher',
+      component: () => import('../views/TeacherView.vue')
+    },
+    {
+      path: '/drop',
+      name: 'drop',
+      component: () => import('../views/DropdownView.vue')
     }
   ]
 })
