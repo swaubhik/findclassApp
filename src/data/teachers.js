be5c4335dfc02c967b3f73dc424f092e71cc257a
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-05-08 21:38:59 +0530

    Create teachers.js

---
+++ src/data/teachers.js
@@ -0,0 +1,24 @@
+const teachers = [
+  {
+    id: 1,
+    code: 'ABP',
+    name: 'Abhijit Padun'
+  },
+  {
+    id: 2,
+    code: 'ABP',
+    name: 'Abhijit Padun'
+  },
+  {
+    id: 3,
+    code: 'ABP',
+    name: 'Abhijit Padun'
+  },
+  {
+    id: 4,
+    code: 'ABP',
+    name: 'Abhijit Padun'
+  }
+]
+
+export default teachers
